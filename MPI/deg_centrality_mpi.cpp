4ab16f4a41d553e14a5202ceb9d3bbef8b2956aa
Kyra Ballard <k.ballard237@msn.com>
2020-05-11 15:40:04 -0400

    Updated degree centrality MPI

--- MPI/deg_centrality_mpi.cpp
+++ MPI/deg_centrality_mpi.cpp
@@ -53,7 +53,6 @@ void deg_centrality(int rank) {
 int main(int argc, char * argv[]){
     MPI_Status status;
     int rank;
-    int size;
     MPI_Init(&argc, &argv);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
